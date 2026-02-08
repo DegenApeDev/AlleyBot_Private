7b3dba3585c460e518eac5ead6e9723cde52fff0
DegenApeDev <degenapedev@gmail.com>
2026-02-07 23:41:35 -0800

    Fix plugin loading: force reload to get latest version

--- plugin_manager.py
+++ plugin_manager.py
@@ -115,7 +115,12 @@ class PluginManager:
                 module_path = f'plugins.{plugin_name}.mcp_plugin'
             else:
                 module_path = f'plugins.{plugin_name}.{plugin_name}'
-            module = importlib.import_module(module_path)
+            
+            # Force reload to get latest version
+            if module_path in sys.modules:
+                module = importlib.reload(sys.modules[module_path])
+            else:
+                module = importlib.import_module(module_path)
             
             # Find plugin class
             plugin_class = None
